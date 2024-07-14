a9693fb7af38731222fca3b1369b3276217f7989
= <mail@tickogrey.com>
2024-07-14 00:13:03 -0700

    basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+// Required Modules
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import 'dotenv/config'
+
+// Custom Modules
 import * as time from "./modules/time.js"
+
+// Setup
+const client = new Client({
+     intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent, 
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessagePolls,
+        GatewayIntentBits.GuildMessageReactions,
+    ] 
+});
+
+// Establish connection
+client.login(process.env.API_DISCORD)
