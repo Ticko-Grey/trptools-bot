21266a650f543af051c1652458b16e02911a2c4e
Luna <mail@tickogrey.com>
2025-01-05 14:37:56 -0700

    improved the shift begin response

--- modules/commands/begin.js
+++ modules/commands/begin.js
@@ -30,10 +30,12 @@ export async function begin(interaction, client) {
         note = ''
     }
 
+    const shiftText = `${nextShift.UID} is starting now. \n \n${note}[Click here to join](https://www.roblox.com/games/start?placeId=2337102976&launchData={"Server":${id}}) \n(or join through the self hosted servers tab)`
+
     const shiftEmbed = new EmbedBuilder()
         .setColor(0x4287f5)
         .setTitle(`Shift announcement`)
-        .setDescription(`${nextShift.UID} is starting now. \n \n${note}[Click here to join](https://www.roblox.com/games/start?placeId=2337102976&launchData={"Server":${id}}) \n(or join through the self hosted servers tab)`)
+        .setDescription(shiftText)
         .setFooter({iconURL: `https://cdn.discordapp.com/icons/${interaction.guild.id}/${interaction.guild.icon}.png`, text: interaction.guild.name + " • Hosted by " + interaction.member.nickname})
 
     await shiftChannel.send({
@@ -59,7 +61,7 @@ export async function begin(interaction, client) {
     const responseEmbed = new EmbedBuilder()
         .setColor(0x69f079)
         .setTitle("Shift announcement created")
-        .setDescription(`Shift has been announced in <#${config.channels.shifts}>`)
+        .setDescription(`Shift has been announced in <#${config.channels.shifts}> \n \nShift text:\n \n\`\`\`\n${shiftText}\n\`\`\``)
 
     interaction.reply({
         embeds: [ responseEmbed],
